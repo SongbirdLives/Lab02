4dd439d32f6f0e91115ac20079228224dcc399e3
g_gio <g_gio@LAPTOP-B88R9TPL.attlocal.net>
2018-10-25 16:43:50 -0700

    testing local branch versus main branch

--- Lab002/src/Hello.java
+++ Lab002/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("giovannigonzale@csus.edu - This line was added by Giovanni Gonzalez");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("just myself - test for local save versus main branch.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
